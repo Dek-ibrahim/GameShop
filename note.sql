d44acc07d939d88e12cc5bcb6a3d2df5610b3e80
dek-ibrahim <deekibraa@gmail.com>
2024-01-21 22:04:29 +0300

    done

--- note.sql
+++ note.sql
@@ -49,7 +49,5 @@ CREATE TABLE Bus (
     ScheduleID INT PRIMARY KEY,
     RouteID INT,
     BusID INT,
-    DayOfWeek VARCHAR(10),
-    FOREIGN KEY (RouteID) REFERENCES Route(RouteID),
-    FOREIGN KEY (BusID) REFER  
+    DayOfWeek VARCHA   
 );
