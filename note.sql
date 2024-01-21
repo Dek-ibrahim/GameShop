82c60a46acd42070644626e01dec3b980e095990
dek-ibrahim <deekibraa@gmail.com>
2024-01-21 22:09:29 +0300

    done

--- note.sql
+++ note.sql
@@ -11,9 +11,6 @@ buss
 
 CREATE TABLE User (
     UserID INT PRIMARY KEY AUTO_INCREMENT,
-    Username VARCHAR(255),
-    P assword VARCHAR(255),
-    Email VARCHAR(255),
-    Phone VAR
+    Userna R 
     
 ); 
