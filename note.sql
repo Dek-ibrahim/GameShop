78220bc1f2b503e394a0564a1edc3550aabd6a0a
dek-ibrahim <deekibraa@gmail.com>
2024-01-21 22:06:10 +0300

    done

--- note.sql
+++ note.sql
@@ -39,4 +39,4 @@ CREATE TABLE Route (
 );
 -- Bus Table
 CREATE TABLE Bus (
-    BusID INT PRIMARY KEY app.use(session({ secret: 'yourSecretHere', resave: true, saveUninitialized: true }));
+    BusID INT PRIMARY KEY app.use(session({ secret: 'yourSecretHere', resave:
