9bbc5f4fb54e14f832e014dc007e4f07025d1e63
dek-ibrahim <deekibraa@gmail.com>
2024-01-29 01:22:48 +0300

    done

--- Bus/huriye/app/Components/Navbar.tsx
+++ Bus/huriye/app/Components/Navbar.tsx
@@ -5,18 +5,17 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-      
       <div className='flex items-center justify-around -mt-[600px]'>
-      <div className='flex items-center justify-around gap-20'> 
-        <img src="logo.png" alt="logo" width={100} height={100} /> 
+        <div className='flex flex-col items-center justify-center sm:flex-row sm:items-center sm:justify-between gap-4 md:gap-8 lg:gap-20'>
+          <img src="logo.png" alt="logo" width={100} height={100} />
 
-        <div className='flex items-center gap-20 text-end'>
+          <div className='flex items-center gap-4 md:gap-8 lg:gap-20'>
             <h2>Home</h2>
             <h2>About</h2>
+          </div>
         </div>
-      </div>
-      
-      <UserButton afterSignOutUrl="/"/>
+
+        <UserButton afterSignOutUrl="/" />
       </div>
     </>
   )
--- Bus/huriye/app/page.tsx
+++ Bus/huriye/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image"; 
 import Navbar from "./Components/Navbar";
+import Booking from "./Components/Booking/Booking";
 
 export default function Home() {
   return (
     <div className="h-screen">
-     
+     <div className="flex gap-6 text-center ml-[200px] translate-y-[140px]">
+      < Booking /> 
+      <div className=" col-span-3">
+        <h1 >location</h1>
+      </div>
+     </div>
     </div>
   );
 }
