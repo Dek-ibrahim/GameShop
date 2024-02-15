d713de6ae4f7cd015c47a8623810194cc6ceba63
dek-ibrahim <deekibraa@gmail.com>
2024-02-15 22:00:56 +0300

    done

--- z.jsx
+++ z.jsx
@@ -1,1 +1,30 @@
-i
+import React from 'react'
+
+function z() {
+  return (
+    <div>z</div>
+  )
+}
+
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export default z
+export defa
